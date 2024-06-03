950cd105737b1635f1aefff663c05e3dffae389b
jwren4170 <jwren4170@att.net>
2024-06-03 08:38:20 -0500

    Add Navbar and converted images to webp

---
+++ app/properties/[id]/page.jsx
@@ -0,0 +1,9 @@
+const PropertyDetails = () => {
+  return (
+    <div>
+      <h1>Propery Details</h1>
+    </div>
+  );
+};
+
+export default PropertyDetails;
---
+++ app/properties/add/page.jsx
@@ -0,0 +1,9 @@
+const AddProperties = () => {
+  return (
+    <div>
+      <h1>Add Property</h1>
+    </div>
+  );
+};
+
+export default AddProperties;
